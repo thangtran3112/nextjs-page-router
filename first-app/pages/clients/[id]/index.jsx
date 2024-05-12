f80f79c6ec8b91b7d7034824679cb0f6eba13b88
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-05-11 18:20:32 -0700

    handling page not found via 404.jsx

--- first-app/pages/clients/[id]/index.jsx
+++ first-app/pages/clients/[id]/index.jsx
@@ -3,9 +3,24 @@ import { useRouter } from "next/router";
 function ClientProjectsPage() {
   const router = useRouter();
   console.log(router.query);
+
+  function loadProjectHandler() {
+    // load data...
+    // router.push("/clients/thang/projectA");
+    // or router.replace("/clients/thang/projectA"); if we don't want to go back
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: {
+        id: router.query.id,
+        clientprojectid: "projectA",
+      },
+    });
+  }
+
   return (
     <div>
       <h1>The Projects for a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
